2d0cbcd1990a2c50f9b59ad632f2775a08913b84
takada291 <tkp291@gmail.com>
2026-01-14 00:26:44 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'tree-survey-v2.2';
+const CACHE_NAME = 'tree-survey-v2.2.1';
 const urlsToCache = [
   './',
   'index.html',
@@ -37,4 +37,5 @@ self.addEventListener('fetch', (event) => {
     })
   );
 });
+
 
